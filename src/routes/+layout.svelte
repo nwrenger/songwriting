199e09b3ab4baf44d67c5657fa2a7622c4759ef1
Nils Wrenger <nils@wrenger.net>
2025-03-01 00:31:16 +0100

    :wrench: Finished migration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-	import Lightswitch from '$lib/components/Lightswitch.svelte';
-	import { Avatar } from '@skeletonlabs/skeleton-svelte';
-
 	let { children } = $props();
 </script>
 
 <div class="px-4 py-5 space-y-4">
-	<!-- Header -->
-	<header>
-		<div class="max-w-5xl mx-auto w-full h-[48px] flex items-center justify-between">
-			<Avatar size="h-full w-[48px]" name="Nils" src="favicon.webp"></Avatar>
-			<Lightswitch />
-		</div>
-	</header>
-	<!-- Main -->
 	{@render children()}
 </div>
